ee4a4a72075125b125a50e2809f2655d21e24be9
Javier Pintado Navarro <javiertoncns03@gmail.com>
2023-03-08 14:36:39 +0100

    Documentacion de Agrupacion
    En este commit he añadido la documentacion de la clase agrupacion

--- src/model/Agrupacion.java
+++ src/model/Agrupacion.java
@@ -30,12 +30,23 @@ public abstract class Agrupacion implements Comparable<Agrupacion> {
 	private Integer agrupacionesTotales;
 	private static Integer cont = 1;
 	
-
+	/**
+	 * Constructor por defecto
+	 * Aumenta el contador de las agrupaciones totales
+	 */
 	public Agrupacion() {
 		setAgrupacionesTotales(cont++);
 	}
 	
-	
+	/**
+	 * Constructor con 5 parametros
+	 * Crea objetos agrupacion, con nombre, autor , autorMusica, autorLetra y el tipo disfraz
+	 * @param nombre <i>Nombre de la agrupacion</i>
+	 * @param autor <i>autor de la agrupacion</i>
+	 * @param autorMusica <i>autor de la musica de la agrupacion</i>
+	 * @param autorLetra <i>autor de la letra de la agrupacion</i>
+	 * @param tipoDisfraz <i>tipo de disfraz de la agrupacion</i>
+	 */
 	public Agrupacion(String nombre, String autor, String autorMusica, String autorLetra, String tipoDisfraz ) {
 		setAgrupacionesTotales(cont++);
 		setNombre(nombre);
@@ -49,16 +60,18 @@ public abstract class Agrupacion implements Comparable<Agrupacion> {
 	
 	/**
 	 * Metodo abstracto que heredan todas las clases hijas
-	 * @see Agrupacion 
+	 * 
 	 * @return <ul>
 	 * 		   		<li>Devuelve una cadena de caracteres</li>
 	 * 		   </ul>
 	 */
 	public abstract String hacerTipo();
 	
-	/**
+	/**Metodo abstracto que heredan todas las clases hijas
 	 * 
-	 * @return
+	 * @return <ul>
+	 * 		   		<li>Devuelve una cadena de caracteres</li>
+	 * 		   </ul>
 	 */
 	public abstract String cantarPresentacion();
 
@@ -111,13 +124,26 @@ public abstract class Agrupacion implements Comparable<Agrupacion> {
 	public void setAgrupacionesTotales(Integer agrupacionesTotales) {
 		this.agrupacionesTotales = agrupacionesTotales;
 	}
-
+	
+	/**
+	 * Metodo implementado de la interfaz Comparable para ordenar por defecto por nombre
+	 * @return <ul>
+	 * 		   		<li>Comparacion del nombre para su ordenacion</li>
+	 * 		   </ul>
+	 */
 	@Override
 	public int compareTo(Agrupacion o) {
 		
 		return this.getNombre().compareTo(o.getNombre()) ;
 	}
 	
+	/**
+	 * Metodo para mostrar por pantalla todos los atributos de la clase
+	 * 
+	 * @return <ul>
+	 * 		   		<li>Cadena con los atributos de la clase</li>
+	 * 		   </ul>
+	 */
 	@Override
 	public String toString() {
 		return "Nombre=" + nombre + ", Autor=" + autor + ", AutorM=" + autorMusica + ", AutorL=" + autorLetra + ", Tipo=" + tipoDisfraz;
