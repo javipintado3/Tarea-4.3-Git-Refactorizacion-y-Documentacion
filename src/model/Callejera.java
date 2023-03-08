049a0607c1f6e4c13c7aac1c864cfb7accc39e8d
Javier Pintado Navarro <javiertoncns03@gmail.com>
2023-03-08 15:29:59 +0100

    Documentación de Callejera
    Documentación de la interfaz callejera

--- src/model/Callejera.java
+++ src/model/Callejera.java
@@ -1,6 +1,18 @@
 package model;
 
+/**
+ * Interfaz Callejera con un metodo
+ * @author JavierPintado
+ * @version 1.1
+ *
+ */
 public interface Callejera {
 	
+	/**
+	 * Metodo que devuelve una cadena
+	 * @return <ul>
+	 * 		   		<li>Un String</li>
+	 * 		   </ul>
+	 */
 	public String amoAescucha();
 }
